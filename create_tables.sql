54a9a968fbb570a5d810f2b0407388e6e88a3cbd
ags2121 <alexsilva88@gmail.com>
2017-03-25 19:56:24 -0400

    write msgs to db

--- create_tables.sql
+++ create_tables.sql
@@ -1,10 +1,12 @@
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
-  `phone_number` varchar(10) NOT NULL,
-  `message_body` varchar(6) NOT NULL,
+  `to` varchar(15) NOT NULL,
+  `from` varchar(15) NOT NULL,
+  `body` varchar(1600) NOT NULL,
   `sent_on` timestamp NULL DEFAULT NULL,
   `processed_on` timestamp NULL DEFAULT NULL,
-  `message_type` varchar(8) DEFAULT NULL,
+  `message_type` varchar(15) DEFAULT NULL,
+  `direction` varchar(15) DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 );
@@ -12,7 +14,7 @@ CREATE TABLE `message` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `uuid` varchar(5) NOT NULL,
-  `phone_number` varchar(10) NOT NULL,
-  `type` varchar(7) NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  `type` varchar(10) NOT NULL,
   PRIMARY KEY (`phone_number`)
 );
--- fetch_messages.py
+++ fetch_messages.py
@@ -2,34 +2,62 @@ from twilio.rest import TwilioRestClient
 import MySQLdb
 import csv
 import yaml
+import pprint 
+pp = pprint.PrettyPrinter(indent=4)
 
-db_config = yaml.load(open('credentials.yaml', 'r'))['db']
+def get_twilio_client(config):
+	return TwilioRestClient(config['account_sid'], config['auth_token'])
 
-def seed_db():
-	conn = MySQLdb.connect(
-		host=db_config['host'], 
-		user=db_config['user'], 
-		passwd=db_config['password'], 
-		db=db_config['database'])
-	cur = conn.cursor()
+def get_db_conn(config):
+	db = MySQLdb.connect(
+		host=config['host'], 
+		user=config['user'], 
+		passwd=config['password'], 
+		db=config['database'])
+	return db, db.cursor()
+
+def insert(rows, table, cursor):
+	columns = ', '.join(map(lambda x: '`%s`' % x, rows[0].keys()))
+	for row in rows:
+		values = ', '.join(map(lambda x: x if x == 'NULL' else "'%s'" % x, row.values()))
+		cursor.execute('INSERT INTO %s(%s) VALUES(%s)' % (table, columns, values))	
+
+def seed_db(db_config):
+	db, cursor = get_db_conn(db_config)
 
 	with open('create_tables.sql', 'r') as queryfile:
 		queries=queryfile.read().split(';')
 
-	[cur.execute(query) for query in queries if query != '\n']
+	[cursor.execute(query) for query in queries if query != '\n']
 
 	users = [row for row in csv.DictReader(open('user.csv'))]
 	messages = [row for row in csv.DictReader(open('message.csv'))]
 
-	def insert(rows, table):
-		columns = ', '.join(map(lambda x: x, rows[0].keys()))
-		print(columns)
-		for row in rows:
-			values = ', '.join(map(lambda x: x if x == 'NULL' else "'%s'" % x, row.values()))
-			cur.execute('INSERT INTO %s(%s) VALUES(%s)' % (table, columns, values))
+	insert(users, 'user', cursor)
+	insert(messages, 'message', cursor)
+	db.commit()
+	cursor.close()
 
-	insert(users, 'user')
-	insert(messages, 'message')
-	conn.commit()
-	cur.close()
+def fetch_messages(db_config):
+	twilio_date_fmt = '%Y-%m-%d'
+	db, cursor = get_db_conn(db_config)
+	cursor.execute('SELECT MAX(processed_on) FROM message')
+	time_of_last_processed_msg = cursor.fetchall()[0][0]
 
+	date_sent = time_of_last_processed_msg.strftime(twilio_date_fmt) if time_of_last_processed_msg != None else None
+	latest_msgs = client.messages.list(DateSent=date_sent)
+
+	msgs_for_db = [{'from': msg.from_, 'to': msg.to, 'body':msg.body, 'direction': msg.direction, 'sent_on': msg.date_sent} for msg in latest_msgs]
+	insert(msgs_for_db, 'message', cursor)
+
+	db.commit()
+	cursor.close()
+	return latest_msgs
+
+config = yaml.load(open('credentials.yaml', 'r'))
+db_config = config['db']
+client = get_twilio_client(config['twilio'])
+
+def main():
+	seed_db(db_config)
+	fetch_messages(db_config)
